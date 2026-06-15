50235ac57508cdb4894702edde01ea9cc6525d9c
kdchnkv home <kadochnikovandrew@gmail.com>
2026-06-15 20:02:08 +0300

    add verify seccess message on order finish page

--- tests/checkOut.spec.ts
+++ tests/checkOut.spec.ts
@@ -8,7 +8,7 @@ import { testData as data } from "@/testData/testData";
 test.describe('verify product order', { tag: '@regresion' }, () => {
 
     test('verify screen on order finish page',
-        { tag: ['@vizual', '@smole'] }, async ({ productInBasket, page }) => {
+        { tag: ['@vizual', '@smoke'] }, async ({ productInBasket, page }) => {
             const basketPage = await new BasketPage(page);
             const checkoutInfoPage = await new CheckoutInfoPage(page);
             const overviewPage = await new OvetviewPage(page);
@@ -23,11 +23,31 @@ test.describe('verify product order', { tag: '@regresion' }, () => {
             await expect(complitePage.comlpleteContent()).toHaveScreenshot();
         })
 
+    test('verify secces message on finish page',
+        { tag: ['@content', '@regress'] }, async ({ productInBasket, page }) => {
+            const basketPage = await new BasketPage(page);
+            const checkoutInfoPage = await new CheckoutInfoPage(page);
+            const overviewPage = await new OvetviewPage(page);
+            const complitePage = await new ComplitePage(page);
+
+            await productInBasket.header.basketIconClick();
+            await basketPage.Checkout();
+            await checkoutInfoPage.fillData();
+            await checkoutInfoPage.btnContinueClick();
+            await overviewPage.clickFinish();
+
+            await expect(complitePage.completeHeader()).
+                toHaveText(data.completeContent.header);
+            await expect(complitePage.completeText()).
+                toHaveText(data.completeContent.text)
+        })
+
+
 });
 
 test.describe('verify information form', () => {
 
-    test('if first name field is empty', async ({ productInBasket, page }) => {
+    test('if name field is empty', async ({ productInBasket, page }) => {
         const basketPage = await new BasketPage(page);
         const checkoutInfoPage = await new CheckoutInfoPage(page);
         const overviewPage = await new OvetviewPage(page);
@@ -42,6 +62,40 @@ test.describe('verify information form', () => {
 
         await expect(checkoutInfoPage.errorMessage()).
             toContainText(data.errorMessage.requerFirstName);
+    })
+
+    test('if last name field is empty', async ({ productInBasket, page }) => {
+        const basketPage = await new BasketPage(page);
+        const checkoutInfoPage = await new CheckoutInfoPage(page);
+        const overviewPage = await new OvetviewPage(page);
+        const complitePage = await new ComplitePage(page);
+
+        await productInBasket.header.basketIconClick();
+        await basketPage.Checkout();
+
+        await checkoutInfoPage.fillFirstJName();
+        await checkoutInfoPage.fillZipCode();
+        await checkoutInfoPage.btnContinueClick();
+
+        await expect(checkoutInfoPage.errorMessage()).
+            toContainText(data.errorMessage.requerLastName);
+    })
+
+    test('if post code field is empty', async ({ productInBasket, page }) => {
+        const basketPage = await new BasketPage(page);
+        const checkoutInfoPage = await new CheckoutInfoPage(page);
+        const overviewPage = await new OvetviewPage(page);
+        const complitePage = await new ComplitePage(page);
+
+        await productInBasket.header.basketIconClick();
+        await basketPage.Checkout();
+
+        await checkoutInfoPage.fillFirstJName();
+        await checkoutInfoPage.fillLastName();
+        await checkoutInfoPage.btnContinueClick();
+
+        await expect(checkoutInfoPage.errorMessage()).
+            toContainText(data.errorMessage.requerPostCode);
     })
 
 });
